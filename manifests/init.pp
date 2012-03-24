eab925695dcf0a915b95ddce7371f05d3bf05593
Surendra Singhi <ssinghi@kreeti.com>
2012-03-24 17:38:34 +0530

    fix ruby dependencies, and update dems install bundler

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class ruby($version = '1.9.2-p318') {
    Exec { path => [ '/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'], logoutput => true,}
 
+   class {'ruby::dependencies'}
    file { "/usr/local/src": ensure => directory }
 
     exec { "wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-${version}.tar.gz":
@@ -14,21 +15,38 @@ class ruby($version = '1.9.2-p318') {
       cwd       => "/usr/local/src",
       creates   => "/usr/local/src/tar-${version}",
       alias     => "untar-ruby-source",
-      subscribe => Exec["ruby-source-tgz"]
+      subscribe => Exec["ruby-source-tgz"],
     }
 
-    exec { "./configure":
+    Exec['untar-ruby-source] => Notify['configure-ruby'],
+
+    exec { "bash configure":
       cwd     => "/usr/local/src/ruby-${version}",
       require => [Exec["untar-ruby-source"], Class['ruby::dependencies']],
+      alias   => 'configure-ruby',
+      refreshonly => true,
       creates => "/usr/local/src/ruby-${version}/config.h",
       before  => Exec["make install"],
     }
 
     exec { "make && make install":
       cwd     => "/usr/local/src/ruby-${version}",
-      alias   => "make install",
-      creates => [ "/usr/local/src/ruby-${version}/ruby",
-                   "/usr/local/bin/ruby" ],
-      require => Exec["./configure"],
+      alias   => "make-install",
+      creates => "/usr/local/bin/ruby",
+      require => Exec["configure-ruby"],
+    }
+
+    exec { "gem update --system":
+      cwd     => "/usr/local/src/ruby-${version}",
+      alias   => "gem-update",
+      created => "/usr/local/bin/gem",
+      require => Exec["make-install"],
+    }
+
+    exec { "gem install bundler":
+      cwd     => "/usr/local/src/ruby-${version}",
+      alias   => "gem-install-bundler",
+      created => "/usr/local/bin/bundle",
+      require => Exec["gem-update"],
     }
 }
